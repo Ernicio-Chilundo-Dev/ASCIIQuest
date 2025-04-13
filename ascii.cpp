df83e2766069ec405321b86cbe5feb3c0a82c8a7
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-04-13 12:33:48 +0200

    funcao fightMonster criada com sucesso ser para determinar o numero de dados que o jogador contraiu durante o jogo assim como o mostro e assim indicar o vencedor

--- ascii.cpp
+++ ascii.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void  DrawIntro(){
+void  drawIntro(){
     cout << R"(
      _____                     _       _            
     |  _  |___ ___ ___ ___ ___| |_ ___| |_ ___ ___  
@@ -24,9 +24,20 @@ int fightMonster(){
         int attack =rand() % 5 + 1;
         int monsterAttack = rand() % 4 +1;
 
-        
+        monsterHP -=attack;
+        playerHP -= monsterAttack;
+
+        cout << "Voce acertou "<<attack<<" de dano.\n";
+        cout << "O mostro te causou "<<monsterAttack<< "de dano. \n";
     }
-    
+
+    if(playerHP > 0){
+        cout << "voce Venceu!\n";
+        return 1;
+    }else{
+        cout << "Voce foi derrotado...\n";
+        return 0;
+    }    
 
 }
 
