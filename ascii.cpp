6395bc71298d415c9b64b841d2c5584ce48fd757
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-04-13 12:09:44 +0200

    desenvolvendo funcao fightMonster

--- ascii.cpp
+++ ascii.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
-#include <
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void  DrawIntro(){
+    cout << R"(
+     _____                     _       _            
+    |  _  |___ ___ ___ ___ ___| |_ ___| |_ ___ ___  
+    |     | .'|  _|  _| . |  _| '_|_ -|  _| .'|  _| 
+    |__|__|__,|___|_| |___|___|_,_|___|_| |__,|_|   
+    )"<<"\n\n";
+}
+
+int fightMonster(){
+    int playerHP =10;
+    int monsterHP = 8;
+
+    cout<<"Um monstro aparece! Prepaare-se appr,para lutar\n";
+
+    while (playerHP > 0 && monsterHP > 0)
+    {
+        int attack =rand() % 5 + 1;
+        int monsterAttack = rand() % 4 +1;
+
+        
+    }
+    
+
+}
+
+int main(){
+
+    return 0;
+}
